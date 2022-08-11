0fb39b81725fb09098110bc0131af020cf5b8927
rogelio1502 <rogelio15021515@gmail.com>
2022-08-11 17:53:40 -0500

    new structure

--- main.py
+++ main.py
@@ -5,48 +5,84 @@ from marker import make_map, process_info_for_marker
 from optimization_routes import call_sync_api, proccess_json_for_gcloud
 
 
-coordinates = json.loads(open("./json/enviaflores_production_direcciones.json").read())
+# start request dummy info
 
 
-deliveries = []
+def get_dummy_deliveries_info(q_deliveries, delivery_block):
+    if q_deliveries > 40 or q_deliveries < 1:
+        raise Exception('"q_deliveries" max value is 40 and min value is 1"')
+    blocks_special_hours = [
+        [8, 9, 10, 11],
+        [10, 11, 12, 13, 14, 15],
+        [16, 17, 18, 19],
+    ]
 
-for i in range(40):
+    coordinates = json.loads(
+        open("./json/enviaflores_production_direcciones.json").read()
+    )
 
-    location_y = float(coordinates[i].get("gm_lattitude"))
-    location_x = float(coordinates[i].get("gm_longitude"))
-    order_id = str(randint(90000, 95000))
+    deliveries = []
 
-    obj = {
-        "order_id": f"demo-{i}",
-        "location_y": location_y,
-        "location_x": location_x,
-    }
+    for i in range(q_deliveries):
 
-    deliveries.append(obj)
+        location_y = float(coordinates[i].get("gm_lattitude"))
+        location_x = float(coordinates[i].get("gm_longitude"))
+        # order_id = str(randint(90000, 95000))
+
+        obj = {
+            "order_id": f"demo-{i}",
+            "location_y": location_y,
+            "location_x": location_x,
+        }
+        if i == 2 or i % 3 == 0 or i == 9:
+            block_special_hour = blocks_special_hours[delivery_block - 1]
+            start_hour = randint(block_special_hour[0], block_special_hour[-1])
+            end_hour = start_hour + 1
+            obj.update(
+                {
+                    "special": {
+                        "hour": {
+                            "start": f"{str(start_hour)}:00:00",
+                            "end": f"{str(end_hour)}:00:00",
+                        },
+                    },
+                }
+            )
+        deliveries.append(obj)
+    return deliveries
 
 
-data_for_request = proccess_json_for_gcloud(
-    {
-        "shipments": deliveries,
-        "drivers": 4,
-        "cost_per_traveled_hour": 25,
-        # "auto_limit": True,
-        "limit_per_driver": 10,
-        "block": 1,
-        "depot_location_y": 25.665667776013077,
-        "depot_location_x": -100.45060983468106,
-    }
-)
+# end request dummy info
 
 
-str_json_response = call_sync_api(json.dumps(data_for_request))
-info_for_marker = process_info_for_marker(json.loads(str_json_response), deliveries)
-make_map(info_for_marker)
-# da = dict(json.loads(str_json_response))
-# routes = da.get("routes")
-# from datetime import datetime, timedelta
+def run():
+    deliveries = get_dummy_deliveries_info(40, 1)
 
-# utc_start_time = "2022-08-09T15:56:20Z"
-# utc_start_time_datetime = datetime.strptime(utc_start_time, "%Y-%m-%dT%H:%M:%SZ")
-# no_utc_start_time_datetime = utc_start_time_datetime - timedelta(hours=5)
-# print(no_utc_start_time_datetime)
+    data_for_request = proccess_json_for_gcloud(
+        {
+            "shipments": deliveries,
+            "drivers": 4,
+            "cost_per_traveled_hour": 25,
+            "auto_limit": True,
+            # "limit_per_driver": 10,
+            "block": 1,
+            "depot_location_y": 25.665667776013077,
+            "depot_location_x": -100.45060983468106,
+        }
+    )
+
+    str_json_response = call_sync_api(json.dumps(data_for_request))
+    info_for_marker = process_info_for_marker(json.loads(str_json_response), deliveries)
+    make_map(info_for_marker)
+    # da = dict(json.loads(str_json_response))
+    # routes = da.get("routes")
+    # from datetime import datetime, timedelta
+
+    # utc_start_time = "2022-08-09T15:56:20Z"
+    # utc_start_time_datetime = datetime.strptime(utc_start_time, "%Y-%m-%dT%H:%M:%SZ")
+    # no_utc_start_time_datetime = utc_start_time_datetime - timedelta(hours=5)
+    # print(no_utc_start_time_datetime)
+
+
+if __name__ == "__main__":
+    run()
--- marker.py
+++ marker.py
@@ -1,13 +1,15 @@
-import json
-from turtle import width
 import folium
 
-colors = ["blue", "white", "yellow", "orange", "rgb(180, 240, 80)"]
+colors = [
+    "#05a49b",
+    "#8277ec",
+    "yellow",
+    "#77ecdc",
+    "rgb(180, 240, 80)",
+]
 
 
 def process_info_for_marker(info: dict, deliveries_locations: list):
-    # print(type(info))
-    # print(info.get("metrics"))
 
     info_for_marker = []
 
@@ -27,11 +29,15 @@ def process_info_for_marker(info: dict, deliveries_locations: list):
                     == i.get("visitLabel"),
                     deliveries_locations,
                 )
+
                 if odd:
 
                     odd = list(odd)
                     x = odd[0].get("location_x")
                     y = odd[0].get("location_y")
+
+                    if odd[0].get("special"):
+                        _visit.update({"special": True})
 
                 _visit.update({"lattitude": y, "longitude": x})
                 _visits.append(_visit)
@@ -41,7 +47,7 @@ def process_info_for_marker(info: dict, deliveries_locations: list):
     return info_for_marker
 
 
-def mark_in_map(map, y, x, tag, color, order=0, main_point=False):
+def mark_in_map(map, y, x, tag, color, order=0, main_point=False, special=False):
     height = "30"
     width = "30"
     color = color
@@ -54,7 +60,7 @@ def mark_in_map(map, y, x, tag, color, order=0, main_point=False):
         text = "Taller"
     div = folium.DivIcon(
         html=(
-            f'<svg height={height} width={width} style="background-color:{color}; border-radius:4px; border: 1px outset black;" >'
+            f'<svg height={height} width={width} style="background-color:{color}; border-radius:16px; border: {"3" if special else "1"}px solid {"red" if special else "black"};" >'
             f"""
             <text x="5" y="10" fill={fill_text}>
                 {f'<tspan x="5" dy="10px" >{text}</tspan>' if main_point else ""}
@@ -68,14 +74,12 @@ def mark_in_map(map, y, x, tag, color, order=0, main_point=False):
     folium.Marker(
         [y, x],
         popup=tag + "\n" + str(order),
-        tooltip=tag,
+        tooltip=str(order),
         icon=div,
     ).add_to(map)
 
 
 def make_map(data: list):
-
-    print(len(data))
 
     my_map = folium.Map(
         location=[25.665667776013077, -100.45060983468106], zoom_start=12
@@ -91,7 +95,7 @@ def make_map(data: list):
     )
 
     for route in data:
-        print(len(route.get("visits")))
+
         c = 0
         for delivery in route.get("visits"):
 
@@ -102,6 +106,7 @@ def make_map(data: list):
                 delivery.get("order_id"),
                 colors[int(route.get("route"))],
                 order=c + 1,
+                special=delivery.get("special"),
             )
             c += 1
 
